73479cc99b9618e1261d137b6155daee6d938dfa
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-01-06 16:38:54 -0400

    add Backend

---
+++ server/models/Ident_types.ts
@@ -0,0 +1,57 @@
+import {
+	Column,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+	CreateDateColumn,
+	OneToMany,
+	JoinColumn,
+	getRepository,
+} from 'typeorm';
+import Users from './Users';
+
+@Entity()
+export default class Ident_types {
+	/*
+	constructor() {
+		setTimeout(async () => {
+			const data: Ident_types[] = [
+				{
+					name: 'V',
+				},
+				{
+					name: 'E',
+				},
+				{
+					name: 'J',
+				},
+				{
+					name: 'R',
+				},
+				{
+					name: 'P',
+				},
+			];
+			//
+			const valid = await getRepository(Ident_types).count({ where: data });
+			if (!valid) await getRepository(Ident_types).save(data);
+		}, 5000);
+	}
+	*/
+
+	@PrimaryGeneratedColumn()
+	id?: number;
+
+	@Column({ nullable: true })
+	name!: string;
+
+	@OneToMany(() => Users, (Users) => Users)
+	@JoinColumn()
+	Users?: Users[];
+
+	@CreateDateColumn({ select: false })
+	createdAt?: Date;
+
+	@UpdateDateColumn({ select: false })
+	updatedAt?: Date;
+}
---
+++ server/models/Users.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn, JoinColumn, Index, ManyToOne } from 'typeorm';
+import Ident_types from './Ident_types';
+
+@Entity()
+@Index(['id_ident_type', 'ident_num'], { unique: true })
+export default class Users {
+	@PrimaryGeneratedColumn()
+	id?: number;
+
+	@Column({ unique: true })
+	email!: string;
+
+	@Column()
+	password!: string;
+
+	@Column()
+	ident_num!: string;
+
+	@ManyToOne(() => Ident_types, (Ident_types) => Ident_types.Users)
+	@JoinColumn({ name: 'id_ident_type' })
+	id_ident_type!: number | Ident_types;
+
+	@Column({ default: 0 })
+	block?: number;
+}
